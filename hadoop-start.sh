85c96bf8c4e40a24d6f7a3b15592c29523d22bd9
vstrath <31084698+vstrath@users.noreply.github.com>
2017-08-22 23:02:54 -0300

    Update hadoop-start.sh

--- hadoop-start.sh
+++ hadoop-start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-/usr/sbin/sshd
-#su hduser -c /usr/local/hadoop/bin/hadoop namenode -format
-#su hduser -c /usr/local/hadoop/sbin/./start-dfs.sh
-#tail -f $HADOOP_HOME/logs/*
+su hduser -c /usr/local/hadoop/bin/hadoop namenode -format
+su hduser -c /usr/local/hadoop/sbin/./start-dfs.sh
+##tail -f $HADOOP_HOME/logs/*
